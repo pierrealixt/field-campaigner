b4db55b6d0ea5c5b3ea9080a639c1ee6c05d9f77
pierrealixt <pierrealix@kartoza.com>
2018-07-17 15:52:09 +0200

    [BUG] error import in this file

--- flask_project/scripts/create_tables.py
+++ flask_project/scripts/create_tables.py
@@ -1,21 +1,17 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+# #!/usr/bin/python
+# # -*- coding: utf-8 -*-
 
-import os
-from sqlalchemy import (
-    create_engine
-)
-from sqlalchemy.ext.declarative import declarative_base
+# import os
+# from sqlalchemy import (
+#     create_engine
+# )
+# from sqlalchemy.ext.declarative import declarative_base
 
-db_location = os.environ['DATABASE_URL']
-engine = create_engine(db_location, echo=True)
-Base = declarative_base()
 
-import sys
-print('create_tablres.py')
-print(sys.path)
+# engine = create_engine(db_location, echo=True)
+# Base = declarative_base()
 
-from flask_project.campaign_manager.models.models import *
+# from flask_project.campaign_manager.models.models import *
 
-Base.metadata.drop_all(engine)
-Base.metadata.create_all(engine)
+# Base.metadata.drop_all(engine)
+# Base.metadata.create_all(engine)
