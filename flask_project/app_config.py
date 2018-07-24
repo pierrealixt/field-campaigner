78073312fb8e41c0c1833c9918eb6d2ec40c3033
pierrealixt <35932320+pierrealixt@users.noreply.github.com>
2018-07-24 23:22:34 +0200

    Revert "Aws eb"

--- flask_project/app_config.py
+++ flask_project/app_config.py
@@ -8,18 +8,9 @@ try:
     from secret import OAUTH_CONSUMER_KEY, OAUTH_SECRET, SENTRY_DSN
 except ImportError:
     THE_SECRET_KEY = os.environ['SECRET_KEY']
-
-    if 'OAUTH_CONSUMER_KEY' in os.environ:
-        OAUTH_CONSUMER_KEY = os.environ['OAUTH_CONSUMER_KEY']
-    else:
-        OAUTH_CONSUMER_KEY = 'SET_OAUTH_CONSUMER_KEY'
-
-    if 'OAUTH_SECRET' in os.environ:
-        OAUTH_SECRET = os.environ['OAUTH_SECRET']
-    else:
-        OAUTH_SECRET = 'SET_OAUTH_SECRET'
-
-    SENTRY_DSN = os.environ['SENTRY_DSN']
+    OAUTH_CONSUMER_KEY = ''
+    OAUTH_SECRET = ''
+    SENTRY_DSN = ''
 
 
 try:
@@ -39,6 +30,7 @@ class Config(object):
     OAUTH_SECRET = OAUTH_SECRET
     SENTRY_DSN = SENTRY_DSN
     MAX_AREA_SIZE = 320000000
+    
 
     # OSMCHA ATTRIBUTES
     _OSMCHA_DOMAIN = 'https://osmcha.mapbox.com/'
@@ -65,7 +57,6 @@ class StagingConfig(Config):
     if 'DATABASE_URL' in os.environ:
         DB_LOCATION = os.environ['DATABASE_URL']
 
-
 class AWSStagingConfig(Config):
     DEBUG = False
 
@@ -75,7 +66,6 @@ class AWSStagingConfig(Config):
             os.environ['RDS_PASSWORD'],
             os.environ['RDS_HOSTNAME'],
             os.environ['RDS_DB_NAME'])
-
 
 class AWSDevelopmentConfig(Config):
     """ AWS Development environment.
@@ -89,7 +79,6 @@ class AWSDevelopmentConfig(Config):
             os.environ['RDS_PASSWORD'],
             os.environ['RDS_HOSTNAME'],
             os.environ['RDS_DB_NAME'])
-
 
 class DevelopmentConfig(Config):
     """Development environment.
@@ -111,5 +100,5 @@ class TestingConfig(Config):
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     if 'TESTDATABASE_URL' in os.environ:
         DB_LOCATION = os.environ['TESTDATABASE_URL']
-
+        
     DRIVER_PATH = os.path.abspath('./campaign_manager/test/chromedriver')
