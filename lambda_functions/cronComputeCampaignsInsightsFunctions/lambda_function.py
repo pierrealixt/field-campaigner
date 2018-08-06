dec77fd1d0241a6e69724ac4f8ea34db812b47aa
pierrealixt <pierrealix@kartoza.com>
2018-08-06 11:27:18 +0200

    wip; list S3 json objects

--- lambda_functions/cronComputeCampaignsInsightsFunctions/lambda_function.py
+++ lambda_functions/cronComputeCampaignsInsightsFunctions/lambda_function.py
@@ -7,17 +7,6 @@ SITE = os.environ['site']  # URL of the site to check, stored in the site enviro
 EXPECTED = os.environ['expected']  # String expected to be on the page, stored in the expected environment variable
 
 
-def validate(res):
-    '''Return False to trigger the canary
-
-    Currently this simply checks whether the EXPECTED string is present.
-    However, you could modify this to perform any number of arbitrary
-    checks on the contents of SITE.
-    '''
-    return EXPECTED in res
-
-
-
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
     bucket = 'fieldcampaigner-data'
